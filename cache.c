6516e23b968fa6eb75d529951e75cdf8fb842f71
Zhannator <zhp3@pitt.edu>
2016-03-29 16:35:39 -0400

    Good Version 1
    Clean Template with Parameters and Prints

--- cache.c
+++ cache.c
@@ -55,14 +55,6 @@ int trace_get_item(struct trace_item **item)
     return 1;
 }
 
-int checkPowerOfTwo(int num){
-    while(((num & 1) == 0) && num > 1) {
-        num >>= 1;
-    }
-    
-    return num == 1;
-}
-
 int main(int argc, char **argv)
 {
     struct trace_item *tr_entry;
@@ -70,54 +62,49 @@ int main(int argc, char **argv)
     char *trace_file_name;
     int trace_view_on, cache_size, block_size;
     int associativity, replacement_policy;
+	enum cache_policy policy;
+	
+	//define default
+	trace_view_on = 1;
+	cache_size = 1; //1 KB
+	block_size = 4; //4 bytes = 1 word
+	associativity = 1; //1-way associativity
+	replacement_policy = 0; //0 for LRU, 1 for FIFO
     
     if (argc == 1) {
         fprintf(stdout, "nUSAGE: tv <trace_file> <switch - any character>n");
         fprintf(stdout, "n(switch) to turn on or off individual item view.nn");
         exit(0);
     }
-    
+   
     trace_file_name = argv[1];
-    trace_view_on = (argc == 3); // What?
-    
-    // here you should extract the cache parameters from the command line
-    
-    cache_size = atoi(argv[4]);
-    block_size = atoi(argv[5]);
-    associativity = atoi(argv[6]);
-    replacement_policy = atoi(argv[7]);
-    
-    // Check that the size of cache, cache block and associativity is a power of two
-    if (checkPowerOfTwo(cache_size)){
-        fprintf(stdout, "nMake sure that the cache size is a power of 2.");
-        fprintf(stdout, "%d is not a power of 2.", cache_size);
-        exit(0);
-    }
-    if (checkPowerOfTwo(block_size)){
-        fprintf(stdout, "nMake sure that the block size is a power of 2.");
-        fprintf(stdout, "%d is not a power of 2.", block_size);
-        exit(0);
-    }
-    if (checkPowerOfTwo(associativity)){
-        fprintf(stdout, "nMake sure that the associativity of the cache sets is a power of 2.");
-        fprintf(stdout, " %d is not a power of 2.", associativity);
-		exit(0);
-    }
 	
-	if ( !(replacement_policy == 0 || replacement_policy == 1) ){
+	if (argc == 7)
+	{
+		trace_view_on = atoi(argv[2]) ;
+		// here you should extract the cache parameters from the command line
+		cache_size = atoi(argv[3]); //in kilobytes
+		block_size = atoi(argv[4]);
+		associativity = atoi(argv[5]);
+		if ((cache_size == 1) || (cache_size%2 != 0) || (block_size%2 != 0) || (associativity == 1) || (associativity%2 != 0)) { //should be restricted to the power of 2
+			fprintf(stdout, "Cache size, block size, and block associativity have to be a power of 2.");
+			exit(0);		
+		}
+		replacement_policy = atoi(argv[6]);
+		if ( !(replacement_policy == 0 || replacement_policy == 1) ){
 		fprintf(stdout, "\nMake sure that you pick either 0 for LRU replacement or 1 for FIFO replacement.");
 		fprintf(stdout, " %d is not a valid number.", replacement_policy);
 		exit(0);
 	}
+	}
     
-    enum cache_policy policy;
     if(replacement_policy){
         policy = FIFO;
     }
     else{
         policy = LRU;
     }
-    
+	
     fprintf(stdout, "n ** opening file %sn", trace_file_name);
     
     trace_fd = fopen(trace_file_name, "rb");
@@ -129,16 +116,33 @@ int main(int argc, char **argv)
     
     trace_init();
     
+	//print back all the parameters
+	printf("\n\nParameters:");
+	printf("\nTrace Name: %s", trace_file_name);
+	printf("\nCache Size: %d KBYTES", cache_size);
+	printf("\nBlock Size: %d BYTES", block_size);
+	printf("\nAssociativity: %d", associativity);
+	if (replacement_policy == 0) {
+		printf("\nReplacement Policy: LRU");
+	}
+	else {
+		printf("\nReplacement Policy: FIFO");			
+	}
+	
     // here should call cache_create(cache_size, block_size, associativity, replacement_policy)
-    create_cache(cache_size, block_size, associativity, policy);
+    //create_cache(cache_size, block_size, associativity, policy);
     
     while(1) {
         size = trace_get_item(&tr_entry);
         
         if (!size) {       /* no more instructions to simulate */
-            printf("+ number of accesses : %d n", accesses);
-            printf("+ number of reads : %d n", read_accesses);
-            printf("+ number of writes : %d n", write_accesses);
+			printf("\n\nResults:");
+			printf("\nCache Accesses: %d", accesses);
+			printf("\nCache Read Accesses: %d", read_accesses);
+			printf("\nCache Write Accesses: %d", write_accesses);
+			printf("\nCache Hits: %d", hits);
+			printf("\nCache Misses: %d", misses);
+			printf("\nCache Writebacks: %d", misses_with_writeback);
             break;
         }
         else{              /* process only loads and stores */;
@@ -156,8 +160,17 @@ int main(int argc, char **argv)
             }
             // based on the value returned, update the statisctics for hits, misses and misses_with_writeback
         }
+		if(trace_view_on == 1) {
+			printf("\n\nTrace:");
+			printf("\nCache Accesses: %d", accesses);
+			printf("\nCache Read Accesses: %d", read_accesses);
+			printf("\nCache Write Accesses: %d", write_accesses);
+			printf("\nCache Hits: %d", hits);
+			printf("\nCache Misses: %d", misses);
+			printf("\nCache Writebacks: %d", misses_with_writeback);
+		}
     }
-    
+	
     trace_uninit();
     
     exit(0);
